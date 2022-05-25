093f681460b1e000eccff6370d958a1f8cf78654
Anna Galytska <anna@Annas-MacBook-Pro.local>
2022-05-25 18:54:22 +0200

    first commit

---
+++ sketch_may25a/sketch_may25a.ino
@@ -0,0 +1,24 @@
+
+
+#define green 2
+#define yellow 3
+#define red 4
+#define button 5
+
+#define interval 7
+
+
+
+
+void setup() {
+pinMode(green, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(red, OUTPUT);
+
+pinMode(buttin, INPUT_PULLUP);
+}
+
+void loop() {
+  digitalWrite(
+
+}

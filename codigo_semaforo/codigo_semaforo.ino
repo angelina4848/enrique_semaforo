9682e34a16210b429ef07a9f43eb370928a583db
Anna Galytska <anna@Annas-MacBook-Pro.local>
2022-05-25 23:06:04 +0200

    low level relay

--- codigo_semaforo/codigo_semaforo.ino
+++ codigo_semaforo/codigo_semaforo.ino
@@ -3,7 +3,7 @@
 #define green 2
 #define yellow 3
 #define red 4
-#define button 5
+#define button 7 
 
 #define interval 7
 
@@ -19,24 +19,24 @@ pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(green, 1);
-  digitalWrite(yellow, 0);
-  digitalWrite(red, 0);
-  if(digitalRead(button) == 0){
   digitalWrite(green, 0);
   digitalWrite(yellow, 1);
-  digitalWrite(red, 0);
-  delay(interval*1000);
-  digitalWrite(green, 0);
+  digitalWrite(red, 1);
+  if(digitalRead(button) == 0){
+  digitalWrite(green, 1);
   digitalWrite(yellow, 0);
   digitalWrite(red, 1);
+  delay(interval*1000);
+  digitalWrite(green, 1);
+  digitalWrite(yellow, 1);
+  digitalWrite(red, 0);
   
   while(digitalRead(button) != 0){
     delay(50);
   }
-  digitalWrite(green, 0);
-  digitalWrite(yellow, 1);
-  digitalWrite(red, 0);
+  digitalWrite(green, 1);
+  digitalWrite(yellow, 0);
+  digitalWrite(red, 1);
   delay(interval*1000);
   }
   
